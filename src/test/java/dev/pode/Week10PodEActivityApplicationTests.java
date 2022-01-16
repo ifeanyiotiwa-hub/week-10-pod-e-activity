f86ea379e127a84e3afecce784250b3fbea83b73
IFEANYICHUKWU OTIWA <ifeanyichukwu.otiwa@decagon.dev>
2022-01-16 21:56:08 +0100

    Maven Configuration files

---
+++ src/test/java/dev/pode/Week10PodEActivityApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.pode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10PodEActivityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
